04d167d7c7002b4230438958a01cabb72142d9cb
savvy898 <your-savvyscribe8@gmail.com>
2026-02-13 06:19:18 +0100

    Fix typescript error for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata = {
   manifest: "/manifest.json", 
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode })
   return (
     <html lang="en">
       <head>
